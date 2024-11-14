f77996384b9acff33db63eff2230eac239219185
Mike Fisher <Michael_Fisher@URMC.Rochester.edu>
2024-11-14 10:33:17 -0500

    style: reformatted with stylua
    Also updated the emacs mode header.

--- .wezterm.lua
+++ .wezterm.lua
@@ -1,12 +1,12 @@
--- -*- mode:lua; -*-
+-- -*- mode:lua-ts; -*-
 -- Pull in the wezterm API
-local wezterm = require 'wezterm'
+local wezterm = require("wezterm")
 
 -- This will hold the configuration.
 local config = wezterm.config_builder()
 
 -- This is where you actually apply your config choices
-config.font = wezterm.font 'Cascadia Code'
+config.font = wezterm.font("Cascadia Code")
 config.initial_rows = 43
 
 -- and finally, return the configuration to wezterm
