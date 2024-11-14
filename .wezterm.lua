55276bb1363e35afbe84c80913e8189434baaf6f
Mike Fisher <Michael_Fisher@URMC.Rochester.edu>
2024-11-14 10:34:12 -0500

    feat: keybinding updates
    - Add some documentation examples about Opt-LEFT/opt-RIGHT
    - Override some undocumented resize commands that collided with emacs undo

--- .wezterm.lua
+++ .wezterm.lua
@@ -9,5 +9,30 @@ local config = wezterm.config_builder()
 config.font = wezterm.font("Cascadia Code")
 config.initial_rows = 43
 
+local act = wezterm.action
+
+config.keys = {
+    -- Rebind OPT-Left, OPT-Right as ALT-b, ALT-f to match Terminal.app
+    {
+        key = "LeftArrow",
+        mods = "OPT",
+        action = act.SendKey({ key = "b", mods = "ALT" }),
+    },
+    {
+        key = "RightArrow",
+        mods = "OPT",
+        action = act.SendKey({ key = "f", mods = "ALT" }),
+    },
+    -- disable some undocumented resize commands
+    { key = "-", mods = "CTRL", action = act.DisableDefaultAssignment },
+    { key = "_", mods = "CTRL", action = act.DisableDefaultAssignment },
+    { key = "-", mods = "SHIFT|CTRL", action = act.DisableDefaultAssignment },
+    { key = "_", mods = "SHIFT|CTRL", action = act.DisableDefaultAssignment },
+    { key = "+", mods = "CTRL", action = act.DisableDefaultAssignment },
+    { key = "=", mods = "CTRL", action = act.DisableDefaultAssignment },
+    { key = "+", mods = "SHIFT|CTRL", action = act.DisableDefaultAssignment },
+    { key = "=", mods = "SHIFT|CTRL", action = act.DisableDefaultAssignment },
+}
+
 -- and finally, return the configuration to wezterm
 return config
