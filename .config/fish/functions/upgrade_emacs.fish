8b6852ae9d60c1ab9cf5f3de0c5280b5b98068a4
Mike Fisher <Michael_Fisher@URMC.Rochester.edu>
2025-06-02 09:12:29 -0400

    fix: remove --with-ctags
    Removed by the emacs-plus maintainer.

--- .config/fish/functions/upgrade_emacs.fish
+++ .config/fish/functions/upgrade_emacs.fish
@@ -3,8 +3,8 @@ function upgrade_emacs
     set -f new_emacsver "emacs-plus@31"
     if brew fetch --HEAD $new_emacsver
         brew uninstall --ignore-dependencies $old_emacsver
-        
-        brew install $new_emacsver --with-ctags --with-retro-gnu-meditate-levitate-icon
+
+        brew install $new_emacsver --with-retro-gnu-meditate-levitate-icon
     else
         echo "Could not fetch $new_emacsver"
     end
